578e36a710e8ad57ce03e3dea180c78dd709dbf7
Kevin Lee <me@mngyuan.com>
2020-06-23 02:13:42 -0700

    routing for basic i18n support

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,8 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const { supportedLanguages } = require('./i18n')
+
+const defaultLang = 'en'
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
@@ -16,6 +19,8 @@ exports.createPages = async ({ graphql, actions }) => {
             node {
               fields {
                 slug
+                directoryName
+                langKey
               }
               frontmatter {
                 title
@@ -31,18 +36,77 @@ exports.createPages = async ({ graphql, actions }) => {
     throw result.errors
   }
 
+  // Create index pages for supported languages
+  Object.keys(supportedLanguages).forEach((langKey) =>
+    createPage({
+      path: langKey === defaultLang ? '/' : `/${langKey}`,
+      component: path.resolve('./src/templates/blog-index.js'),
+      context: {
+        langKey,
+      },
+    })
+  )
+
   // Create blog posts pages.
   const posts = result.data.allMarkdownRemark.edges
 
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
+  const translationsByDirectory = posts.reduce((res, post) => {
+    const directoryName = post.node.fields.directoryName
+    const langKey = post.node.fields.langKey
+
+    if (!langKey || !directoryName || langKey === 'en') {
+      return res
+    }
+
+    res[directoryName] = [...(res[directoryName] || []), langKey]
+    return res
+  })
+
+  const defaultLangPosts = posts.filter(
+    (post) => post.node.fields.langKey === defaultLang
+  )
+
+  defaultLangPosts.forEach((post, index) => {
+    const previous =
+      index === defaultLangPosts.length - 1
+        ? null
+        : defaultLangPosts[index + 1].node
+    const next = index === 0 ? null : defaultLangPosts[index - 1].node
+    const translations =
+      translationsByDirectory[post.node.fields.directoryName] || []
 
     createPage({
       path: post.node.fields.slug,
       component: blogPost,
       context: {
         slug: post.node.fields.slug,
+        previous,
+        next,
+        translations,
+      },
+    })
+  })
+
+  const translatedPosts = posts.filter(
+    (post) => post.node.fields.langKey !== defaultLang
+  )
+
+  translatedPosts.forEach((post, index) => {
+    const curLangPosts = posts.filter(
+      (p) => p.node.fields.langKey === post.node.fields.langKey
+    )
+    const previous =
+      index === curLangPosts.length - 1 ? null : curLangPosts[index + 1].node
+    const next = index === 0 ? null : curLangPosts[index - 1].node
+    const translations =
+      translationsByDirectory[post.node.fields.directoryName] || []
+
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPost,
+      context: {
+        slug: post.node.fields.slug,
+        translations,
         previous,
         next,
       },
@@ -54,11 +118,10 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
     createNodeField({
-      name: `slug`,
+      name: `directoryName`,
       node,
-      value,
+      value: path.basename(path.dirname(node.fileAbsolutePath)),
     })
   }
 }
---
+++ i18n.js
@@ -0,0 +1,6 @@
+exports.supportedLanguages = {
+  en: 'English',
+  'zh-hant': '繁體中文',
+  'zh-hans': '简体中文',
+  ja: '日本語',
+}
